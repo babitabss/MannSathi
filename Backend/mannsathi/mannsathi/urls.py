7071b0d975c22f7e70f3066a9ffe0d77bca35df7
Babita Acharya <itsssbabita221@gmail.com>
2026-07-12 19:44:56 +0545

    Add homepage redirect to login

--- Backend/mannsathi/mannsathi/urls.py
+++ Backend/mannsathi/mannsathi/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,6 +12,7 @@ urlpatterns = [
     path('', include('community.urls')),
     path('', include('chat.urls')),
     path('', include('notifications.urls')),
+    path('', lambda request: redirect('login')),
 ]
 
 if settings.DEBUG:
